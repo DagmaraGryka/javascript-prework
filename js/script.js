fbb29b60e0cbc0745c39df8fff932b067ae6c930
Dagmara Gryka <dagagryka@gmail.com>
2020-12-18 14:46:12 +0100

    add files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+//Functions
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
