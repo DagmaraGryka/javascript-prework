001c5cc8099b2a4801f6f873259229499686058b
Dagmara Gryka <dagagryka@gmail.com>
2020-12-21 16:28:41 +0100

    add files

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
+
+
+  var playerMove, playerInput;
+  playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  console.log('Wpisana odpowiedź to: ' + playerInput);
+  if (playerInput == '1') {
+    playerMove = 'kamień';
+  } else {
+    playerMove = 'nieznany ruch';
+  }
+  printMessage('Twój ruch: ' + playerMove);
+  if (playerInput == '2') {
+    playerMove = 'papier';
+  } else {
+    playerMove = 'nieznany ruch';
+  }
+  printMessage('Twój ruch: ' + playerMove);
+  if (playerInput == '3') {
+    playerMove = 'nozyce';
+  } else {
+    playerMove = 'nieznany ruch';
+  }
+  printMessage('Twój ruch: ' + playerMove);
