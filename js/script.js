4ecd226e6c162361704f9592a77242bc89b88541
Dagmara Gryka <dagagryka@gmail.com>
2020-12-21 17:10:40 +0100

    change file

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
 
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
 
   var playerMove, playerInput;
   playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -15,18 +19,18 @@ printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygr
   if (playerInput == '1') {
     playerMove = 'kamień';
   } else {
-    playerMove = 'nieznany ruch';
+    playerMove = randomNumber;
   }
   printMessage('Twój ruch: ' + playerMove);
   if (playerInput == '2') {
     playerMove = 'papier';
   } else {
-    playerMove = 'nieznany ruch';
+    playerMove = randomNumber;
   }
   printMessage('Twój ruch: ' + playerMove);
   if (playerInput == '3') {
     playerMove = 'nozyce';
   } else {
-    playerMove = 'nieznany ruch';
+    playerMove = randomNumber;
   }
   printMessage('Twój ruch: ' + playerMove);
