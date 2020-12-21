5cbd74cda305dc95df09b9af59421a6a46156c1d
Dagmara Gryka <dagagryka@gmail.com>
2020-12-21 20:05:09 +0100

    change and add function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
 //Functions
 
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
 
 var randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 printMessage('Wylosowana liczba to: ' + randomNumber);
 
 
-  var playerMove, playerInput;
-  playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-  console.log('Wpisana odpowiedź to: ' + playerInput);
-  if (playerInput == '1') {
-    playerMove = 'kamień';
-  } else {
-    playerMove = randomNumber;
-  }
-  printMessage('Twój ruch: ' + playerMove);
-  if (playerInput == '2') {
-    playerMove = 'papier';
-  } else {
-    playerMove = randomNumber;
-  }
-  printMessage('Twój ruch: ' + playerMove);
-  if (playerInput == '3') {
-    playerMove = 'nozyce';
-  } else {
-    playerMove = randomNumber;
-  }
-  printMessage('Twój ruch: ' + playerMove);
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  
+console.log('Wpisana odpowiedź to: ' + playerInput);
+  
+function getMoveName(moveId) {
+    if (moveId == '1') {
+        return 'kamień';
+    } else if (moveId == '2')
+        return 'papier';
+    } else if (moveId == '3')
+        return 'nozyce';
+    } else {
+        return 'nieznay ruch'
+    }
+}
+  
+playerMove = getMoveName(playerInput);
+console.log('Ruch gracza:', playerMove);
+
+computerMove = getMoveName(randomNumber);
+console.log('Ruch komputera:', computerMove);
+
+if (playerMove == 'kamień' && computerMove == 'papier') {
+    printMessage('Wygrywa komputer');
+} else if (playerMove == 'papier' && computerMove == 'nozyce') {
+    printMessage('Wygrywa komputer');
+} else if (playerMove == 'nozyce' && computerMove == 'kamień') {
+    printMessage('Wygrywa komputer');
+} else if (playerMove == computerMove) {
+    printMessage('Remis');
+} else if (playerMove == 'nozyce' && 'Nieznany ruch') {
+    printMessage('Bledny ruch');
+} else {
+    printMessage('Wygral gracz')
+}
